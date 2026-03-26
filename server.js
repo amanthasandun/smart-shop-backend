8aac3ebd608ac48f2ad95e940468b9cfdf5ed2f8
amantha2002 <amanthasandunaa@gmail.com>
2026-03-26 21:05:41 +0530

    create the backend structure and the mongodb model file

---
+++ server.js
@@ -0,0 +1,24 @@
+import cookieParser from "cookie-parser"
+import express from "express"
+import cors from "cors"
+import connectdb from "./models/db.js"
+import  "dotenv/config"
+
+const app = express()
+const port = process.env.PORT || 4000
+
+await connectdb()
+
+const allowedOrigins = ['https://localhost:5173']
+
+app.use(express.json()) // this is use to make the json format in the proper way
+app.use(cookieParser()) //eads cookies from the request .. Stores them in the req.cookies
+app.use(cors({origin : allowedOrigins , credentials: true   }))
+
+app.get('/', (req, res) => 
+    res.send("Api is working ")
+)
+
+app.listen(port, () => {
+    console.log(`Server is successfully running on http://localhost:${port}`)
+})
