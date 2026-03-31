9b78ee4d9bfb5e099a01656c4ffc253372339a28
amantha2002 <amanthasandunaa@gmail.com>
2026-03-31 21:28:33 +0530

    create the user authentication method in the usercontroller file

--- controllers/userController.js
+++ controllers/userController.js
@@ -79,3 +79,18 @@ export const login = async(req , res) =>{
         
     }
 }
+
+
+// check Auth : /api/user/is-Auth
+
+export const isAuth = async (req , res)=>{
+    try {
+        const {userId} = req.body 
+        const user = await User.findById(userId).select("-password")
+        return res.json({success: true  , user})
+    } catch (error) {
+        console.log(error.message);
+        res.json({success : false , message : error.message})
+        
+    }
+}
