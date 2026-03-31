5699ddd37cd2b8e804ef929372f25e63d0255671
amantha2002 <amanthasandunaa@gmail.com>
2026-03-31 21:41:03 +0530

    create the user logout fnuncitonality

--- controllers/userController.js
+++ controllers/userController.js
@@ -65,7 +65,7 @@ export const login = async(req , res) =>{
 
         const token = jwt.sign({id:user._id}, process.env.JWT_SECRET , {expiresIn : "7d"})
 
-        res.cookie('token' , token , {
+        res.cookie('token' , token , { //res.cookie(name of the token, value, options that related to the cookie)
             httpOnly : true , // prevent javascript to access the cookie 
             sameSite: process.env.NODE_ENV === "production" ? "strict" : "lax", // CSRF protection
             maxAge : 7 * 24 * 60 * 60 * 1000 //      Cookie expire time
@@ -92,5 +92,22 @@ export const isAuth = async (req , res)=>{
         console.log(error.message);
         res.json({success : false , message : error.message})
         
+    }
+}
+
+// logout : /api/user/logout
+
+export const logout = async (req , res )=>{
+    try {
+        res.clearCookie('token' , {
+            httpOnly : true , 
+            secure : process.env.NODE_ENV ==="production" , 
+            sameSite : process.env.NODE_ENV === "produciton" ? "none" : "strict" , 
+        })
+
+        return res.json({success : true  , message : " logged out "})
+    } catch (error) {
+        console.log(error.message) ; 
+        res.json({success : false , message : error.message })
     }
 }
