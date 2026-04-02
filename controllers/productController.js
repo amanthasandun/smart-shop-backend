d1e9e20bd17c5f521248be12dedb6ad3a74049df
amantha2002 <amanthasandunaa@gmail.com>
2026-04-02 10:45:46 +0530

    create the funciton that fetch the all th eproducts form the data base

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,4 +1,5 @@
 import { v2 as cloudinary } from "cloudinary";
+import Product from "../models/product.js"
 // add product : /api/product/add
 export const addProduct = async (req , res)=>{
     try {
@@ -22,8 +23,14 @@ export const addProduct = async (req , res)=>{
 }
 
 // get product : /api/product/list
-export const productList = (req , res)=>{
-    
+export const productList = async (req , res)=>{
+    try {
+        const product = await Product.find({})
+        res.json({ success : true , product})
+    } catch (error) {
+        console.log(error.message);
+        res.json({success : false  , message : error.message})
+    }   
 }
 
 // get product by id : /api/product/id
