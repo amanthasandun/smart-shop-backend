2fb601b1db33a55797051e74850fbec95ae5c380
amantha2002 <amanthasandunaa@gmail.com>
2026-04-03 20:15:32 +0530

    create the order Schema

---
+++ models/orders.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose"
+
+const orderSchema = new mongoose.Schema({
+    userId : {type : string , required : true , ref : "user" },
+    items : [{
+        product : {type : String , required : true , ref : "user"},
+        quantity : {type : Number , required : true},
+    }] ,
+    amount : {type : Number , required : true},
+    address : {type : Number , required : true , ref : "address"},
+    status : {type : Number , default : "order placed"},
+    paymentType : {type : Number , required : true},
+    isPaid : {type : Boolean , required : true , default : false },
+},{
+    timestamps : true 
+})
+
+const Order = mongoose.models.order || mongoose.model("order" , orderSchema)
+
+export default Order 
