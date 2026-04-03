ca033a634600d039176b72ef39a7d4b3b0f38afc
amantha2002 <amanthasandunaa@gmail.com>
2026-04-04 03:00:35 +0530

    create the get all orders function in the ordercontroller

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -47,10 +47,22 @@ export const getUserOrders = async (req , res ) => {
             userId , // Get orders that belong to this specific user
             $or : [{paymentType : "COD" } , {isPaid : true}]  // Get orders where: (paymentType = COD) OR (isPaid = true)
         }).populate("items.product address").sort({createdAt : -1}) //populate() = "replace ID with actual document from another collection using the ref use id as the items.product and the address"
-                                                                    // -1 → newest first  ,, 1 - olderst first we canuse createdAt , updateAt because of the use of timestamps
+                                                                    // -1 → newest first  , 1 - olderst first we canuse createdAt , updateAt because of the use of timestamps
 
         res.json({success : true , orders})
 
+    } catch (error) {
+        res.json({success : false  , message : error.message})
+    }
+}
+
+// get the all the order (for seller  , admin ) :   /api/order/seller/
+export const  getAllOrders = async (req , res ) =>{
+    try {
+        const orders = (await Order.find({
+            $or : [{paymentType : "COD"} , {isPaid : true}]   // "At least ONE of the conditions must be true"
+        }).populate("items.product address")).sort({createdAt : -1})
+        res.json({success : true , orders})
     } catch (error) {
         res.json({success : false  , message : error.message})
     }
